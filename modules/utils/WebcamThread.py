93ed84a8a3aafa70a2d7049fafa1cfc6405e52f0
Vincent LAMBERT <sasuke6sackboy@gmail.com>
2024-04-09 10:24:43 +0200

    reloading

--- modules/utils/WebcamThread.py
+++ modules/utils/WebcamThread.py
@@ -69,8 +69,8 @@ class WebcamThread(QThread):
     def stopWebcam(self):
         if self.cap!=None and self.cap.isOpened():
             self.cap.release()
-        else:
-            raise Exception("Cannot stop webcam, it is not running")
+        # else:
+        #     raise Exception("Cannot stop webcam, it is not running")
 
     def startWebcam(self):
         self.cap = cv2.VideoCapture(0)
